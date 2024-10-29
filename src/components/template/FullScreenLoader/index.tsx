2dbfc87662b07340ca16af7a022ed30dd467cd3d
Hanief Burhanuddin <haniefburhan17@gmail.com>
2024-10-29 15:16:47 +0700

    chore: update full screen loader

--- src/components/template/FullScreenLoader/index.tsx
+++ src/components/template/FullScreenLoader/index.tsx
@@ -4,15 +4,17 @@
 import Loading from '@/components/template/Loading';
 import { useKeepLogin } from '@/services/user/login/hooks/useKeepLogin';
 import { useEffect } from 'react';
+import { useGetStore } from '@/services/store/hooks/useGetStore';
 
 const FullScreenLoader = () => {
   const { keepLoginMutation, status } = useKeepLogin();
+  const { storeDataLoading } = useGetStore();
 
   useEffect(() => {
     keepLoginMutation();
   }, [keepLoginMutation]);
 
-  if (status === 'pending' || status === 'idle') {
+  if (status === 'pending' || status === 'idle' || storeDataLoading) {
     return (
       <div className='fixed inset-0 z-50 flex items-center justify-center bg-white'>
         <Loading />
